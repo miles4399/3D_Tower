4ab76421e6389fa1390dbfe60f8612b5e08bb246
miles4399 <miles4399@gmail.com>
2025-06-03 16:12:08 -0700

    update to place with cursor

--- Assets/Scripts/PlaceObject.cs
+++ Assets/Scripts/PlaceObject.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class PlaceObject : MonoBehaviour
 {
     [SerializeField] private GameObject _placeable;
+
+    private Camera _camera;
+    private Vector3 _cursorPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _camera = Camera.main;
     }
 
     public void Place()
     {
         //Debug.Log("Placing block");
-        GameObject placeable = Instantiate(_placeable, Vector3.zero, Quaternion.identity);
+        GameObject placeable = Instantiate(_placeable, _cursorPos.Flatten(), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            _cursorPos = hit.point;
+        }
     }
 }
